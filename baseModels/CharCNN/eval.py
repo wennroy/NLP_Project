102984a3d53e90b1d8586a9282640a5ea04f9a5c
Haitao Huang <v0rt3xh@gmail.com>
2022-04-24 12:11:17 -0500

    Formatted with Black

--- baseModels/CharCNN/eval.py
+++ baseModels/CharCNN/eval.py
@@ -1,20 +1,22 @@
 from sklearn import metrics
 import numpy as np
 
+
 def get_evaluation(y_true, y_prob, list_metrics):
     y_pred = np.argmax(y_prob, -1)
     output = {}
-    if 'accuracy' in list_metrics:
-        output['accuracy'] = metrics.accuracy_score(y_true, y_pred)
-    if 'loss' in list_metrics:
+    if "accuracy" in list_metrics:
+        output["accuracy"] = metrics.accuracy_score(y_true, y_pred)
+    if "loss" in list_metrics:
         try:
-            output['loss'] = metrics.log_loss(y_true, y_prob)
+            output["loss"] = metrics.log_loss(y_true, y_prob)
         except ValueError:
-            output['loss'] = -1
-    if 'confusion_matrix' in list_metrics:
-        output['confusion_matrix'] = str(metrics.confusion_matrix(y_true, y_pred))
-        output['confusion_mat_original'] = metrics.confusion_matrix(y_true, y_pred)
-        output['f1scores'] = metrics.f1_score(y_true,y_pred,average='macro')
+            output["loss"] = -1
+    if "confusion_matrix" in list_metrics:
+        output["confusion_matrix"] = str(metrics.confusion_matrix(y_true, y_pred))
+        output["confusion_mat_original"] = metrics.confusion_matrix(y_true, y_pred)
+        output["f1scores"] = metrics.f1_score(y_true, y_pred, average="macro")
     return output
+
 
 # def eval_best_model()
